e7d5b6991f35923cff1b1916e99dada010c0604e
Night-Traders-Dev <quegmeister@gmail.com>
2026-05-21 14:59:38 -0400

    refactor: remove unused EFI file info GUID from FAT32 driver

--- sageos_build/kernel/fs/fat32.c
+++ sageos_build/kernel/fs/fat32.c
@@ -736,11 +736,6 @@ typedef struct EfiFileProtocol {
     void *Flush;
 } EfiFileProtocol;
 
-static const uint8_t g_efi_file_info_guid[16] = {
-    0x92, 0x6E, 0x57, 0x09, 0x3F, 0x6D, 0xD2, 0x11,
-    0x8E, 0x39, 0x00, 0xA0, 0xC9, 0x69, 0x72, 0x3B
-};
-
 extern SageOSBootInfo *kernel_get_boot_info(void);
 
 static void ascii_to_utf16_path(const char *src, uint16_t *dst, int max_len) {
